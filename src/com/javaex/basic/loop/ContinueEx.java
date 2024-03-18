d69c409fcc2acc4cbfe66975c2e7b44a1787a3f0
vcsjw <vcsjw@192.168.100.33>
2024-03-18 15:41:16 +0900

    commit 수정

--- src/com/javaex/basic/loop/ContinueEx.java
+++ src/com/javaex/basic/loop/ContinueEx.java
@@ -5,7 +5,8 @@ public class ContinueEx {
 		// 2의 배수와 3의 배수는 출력 안함
 		// 나머지는 출력
 		// for문, continue문 사용
-	public static void main(String[] args) {
+	public static void main(String[] args) {  //여기선 스캐너 import하고열지 않는 이유???
+		
 		for (int i = 1; i <= 20; i += 1) { // for문은 반복문 일정한 횟수를 정해놓고 계속 반복하는거야 계속 로테이트개념.. 20번 돌고 if내용을 실행하겟다
 			if (i % 2 == 0 || i % 3 == 0)
 				continue; // 다음 루프로				
