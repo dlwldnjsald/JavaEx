92bcd5572a6d8774844791945d63d4c7dc9e0ccb
806-26 <806-26@DESKTOP-8V02G4F>
2024-03-29 16:10:55 +0900

    commit and push on 0329 16:10

--- src/com/javaex/exception/ThrowsExceptEx.java
+++ src/com/javaex/exception/ThrowsExceptEx.java
@@ -2,10 +2,11 @@ package com.javaex.exception;
 
 import java.io.IOException;
 
+
+
 // CheckedException: 반드시 예외처리가 필요하다
 //try catch 문으로 처리하거나
 //아니면 throw 로 예외 던지기를 한 경우에
-
 public class ThrowsExceptEx {
 
 	public static void main(String[] args) {
@@ -14,14 +15,26 @@ public class ThrowsExceptEx {
 		
 		//객체 생성해보기-------------------------------------
 		ThrowsExcept except = new ThrowsExcept();
+		//CheckedException은 반드시 예외처리 해줘야한다
 		
-//		except.executeExcept();  //CheckedException은 반드시 예외처리 해줘야한다
 		try {
+			except.divide(100,0);
+			except.executeRuntimeException();  
 			except.executeExcept();
 			
-		} catch (IOException e) {
+		} catch (IOException e) {//------------------------
 //			e.printStackTrace();
 			System.err.println(e.getMessage());
+			;
+			
+		} catch (CustomerArithException e) {//--------------
+			System.err.println(e.getMessage());
+			System.err.println("나누어지는 수: " + e.getNum1());
+			System.err.println("나누는 수: " + e.getNum2());
+		
+		} catch (RuntimeException e) {	//------------------
+			System.err.println(e.getMessage());
+		
 		}
 
 
