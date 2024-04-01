cfecff1bdf2a4fe6d87f3d8b4b7b8d6c5a57ab9e
806-26 <806-26@DESKTOP-8V02G4F>
2024-04-01 17:19:53 +0900

    commit on 0401 17:19

--- src/com/javaex/api/objectclass/ex4/CloneTest.java
+++ src/com/javaex/api/objectclass/ex4/CloneTest.java
@@ -7,8 +7,23 @@ public class CloneTest {
 		Scoreboard s1 = new Scoreboard(new int[] {10, 20, 30, 40});
 		System.out.println("s1=" + s1);
 		
+		//복제본 생성 [얕은복제의 결과 ]
+		Scoreboard s2 = s1.getClone();
+		System.out.println("s2=" + s2);
+		
+		System.out.println("s1 == s2 ? " + (s1 == s2)); //false 전혀 다른 객체임을 알려줌
 		
 		
+		
+		s1.getScores()[0] = 50;	//내부 참조 필드의 값을 변경할 경우?
+		//s1의 scores[]의 0번 인덱스를 50으로 넣어줄경우 
+		System.out.println("s2=" + s2);
+		//s2를 출력하면 값이 변경된것을 알수 있음 그래서 아래와 같이
+		
+		System.out.println("s1.scores == s2.scores ? " +
+				(s1.getScores() == s2.getScores()));
+				//s1.scores, s2.scores는 같은 참조 주소인가를 확인 해보기 // 트루로 나올것.
+
 		
 	}
 
--- src/com/javaex/api/objectclass/ex4/Scoreboard.java
+++ src/com/javaex/api/objectclass/ex4/Scoreboard.java
@@ -1,21 +1,82 @@
+
+//1)얕은 복제
+//package com.javaex.api.objectclass.ex4;
+//// alt shift s 후 source항묙에서 바로 찾아 만들수 있다
+//
+//import java.util.Arrays;
+//
+////clone 메서드 이용을 위해 Cloneable 인터페이스를 구현 (아래코드와 같이 작성)------///
+//public class Scoreboard implements Cloneable {
+//	
+//	//field
+//	private int scores[]; 	//배열 선언후 생성자
+//	
+//	//생성자
+//	public Scoreboard(int[] scores) {
+//		super();
+//		this.scores = scores;
+//	}
+//
+//	//getters,setters 
+//	public int[] getScores() {
+//		return scores;
+//	}
+//
+//	public void setScores(int[] scores) {
+//		this.scores = scores;
+//	}
+//
+//	
+//	//toString  override
+//	@Override
+//	public String toString() {
+//		return "Scoreboard [scores=" + Arrays.toString(scores) + "]";
+//	}
+//	
+//	//복제본 생성 메서드 --------------------------------------------///
+//	public Scoreboard getClone() {
+//		
+//		//새로운 인스턴스를 저장할 변수를 선언하자(아래)
+//		Scoreboard clone = null;  		//우선 참조데이터는 null 빈값으로 설정
+//		
+//		//얕은복제시도 
+//		try {
+//			clone = (Scoreboard)clone();
+//			
+//		} catch (CloneNotSupportedException e) {
+//			e.printStackTrace();//예외 로그 출력
+//		}
+//		//return
+//		return clone;
+//	}
+//	
+//	
+//	
+//
+
+//=========================================================================================
+//2)깊은복제
+//연결을 끊고 클론을 오버라이드하는방법?을 구현해낼수 밖에 ..?
+//깊은 복제 : 참조 
+
 package com.javaex.api.objectclass.ex4;
 // alt shift s 후 source항묙에서 바로 찾아 만들수 있다
 
 import java.util.Arrays;
 
-//clone 메서드 이용을 위해 Cloneable 인터페이스를 구현 (아래코드와 같이 작성)------///
+//clone 메서드 이용을 위해 Cloneable 인터페이스를 구현 (아래코드와 같이 작성)
 public class Scoreboard implements Cloneable {
-	
-	//field
-	private int scores[]; 	//배열 선언후 생성자
-	
-	//생성자
+
+	// field
+	private int scores[]; // 배열 선언후 생성자
+
+	// 생성자
 	public Scoreboard(int[] scores) {
 		super();
 		this.scores = scores;
 	}
 
-	//getters,setters 
+	// getters,setters
 	public int[] getScores() {
 		return scores;
 	}
@@ -24,30 +85,42 @@ public class Scoreboard implements Cloneable {
 		this.scores = scores;
 	}
 
-	
-	//toString  override
+	// toString override
 	@Override
 	public String toString() {
 		return "Scoreboard [scores=" + Arrays.toString(scores) + "]";
 	}
-	
-	//복제본 생성 메서드 --------------------------------------------///
+
+	// 복제본 생성 메서드 --
 	public Scoreboard getClone() {
-		
-		//새로운 인스턴스를 저장할 변수를 선언하자(아래)
-		Scoreboard clone = null;  		//우선 참조데이터는 null 빈값으로 설정
-		
-		//얕은복제시도 
+
+		// 새로운 인스턴스를 저장할 변수를 선언하자(아래)
+		Scoreboard clone = null; // 우선 참조데이터는 null 빈값으로 설정
+
+		// 얕은복제시도
 		try {
-			clone = (Scoreboard)clone();
-			
+			clone = (Scoreboard) clone();
+
 		} catch (CloneNotSupportedException e) {
-			e.printStackTrace();//예외 로그 출력
+			e.printStackTrace();// 예외 로그 출력
 		}
-		//return
+		// return
 		return clone;
 	}
-	
-	
-	
+
+//	@Override // --------------------------------------------------------///
+	protected Object clone() throws CloneNotSupportedException 	{
+		// 깊은 복제를 위해 내부 참조 데이터를 복제하기 //->(배열을 새로 만들어서 데이터 할당해주기)
+
+		// 먼저 얕은 복제 시도----------------------------------------------///
+		Scoreboard clone = (Scoreboard)super.clone(); // Object가 갖고있는 클론 메서드를 호풀
+		// return super.clone();
+		
+		// 내부 참조 객체를 복제 시도-----------------------------------------///
+		clone.scores = Arrays.copyOf(scores, scores.length);
+
+		return clone; // ----------------///만들어진 복제본을 반환
+		
+	}
+
 }
