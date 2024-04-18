1013863a95263755f98daec3654d88b4f548db47
806-26 <806-26@DESKTOP-8V02G4F>
2024-04-18 15:05:02 +0900

    수정 on 0418 15:04

--- src/com/javaex/looppractice/LoopPractice.java
+++ src/com/javaex/looppractice/LoopPractice.java
@@ -1,4 +1,65 @@
-//101페이지//
+package com.javaex.looppractice;
+
+public class LoopPractice {
+
+	public static void main(String[] args) {
+//		forGugu();
+//		whileGugu();
+//		forStar();
+		whileStar();
+	}
+
+	public static void forGugu() {
+		for (int dan = 2; dan <= 9; dan++) {
+			// 단수 돌리기: 2, 3, 4, ... , 9
+			for (int num = 1; num <= 9; num++) {
+				System.out.println(dan + "*" + num + "=" + dan * num);
+			}
+		}
+	}
+
+	public static void whileGugu() {
+		int dan = 2, num;
+
+		while (dan <= 9) {
+			num = 1;
+			while (num <= 9) {
+				System.out.println(dan + "*" + num + "=" + 
+									dan * num);
+				num += 1;
+			}
+			dan += 1;
+		}
+	}
+
+	public static void forStar() {
+		//	행루프 1 ~ 6
+		for (int row = 1; row <= 6; row ++) {
+			// 열루프 : 1 ~ 현재 row
+			for (int col = 1; col <= row; col ++) {
+				System.out.print("*");
+			}
+			System.out.println();	//	강제 개행
+		}
+	}
+
+	public static void whileStar() {
+		int row = 1, col;
+		
+		while (row <= 6) {
+			col = 1;
+			//	행루프 : 1 ~ row
+			while (col <= row) {
+				System.out.print("*");
+				col += 1;
+			}
+			System.out.println();
+				
+				
+			row += 1;
+		}
+	}
+}//101페이지//
 
 //package com.javaex.practice ;
 //public class practicesLoop {
@@ -176,6 +237,7 @@
 //		}	
 //	}
 //}
+
 
 
 
